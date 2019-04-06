81b4c710a119d6679eb94f00526d90d664d2ac69
Dylan Tran <dylantran@berkeley.edu>
2019-04-05 23:37:12 -0700

    completed preliminary smart contract functionality

--- smartcontract/contracts/Review.sol
+++ smartcontract/contracts/Review.sol
@@ -7,18 +7,18 @@ contract Review {
         string location; // maybe change later
         string title;
         uint numTokens;
-        string[] tags;
+        //string[] tags;
         address author;
+        bool ready;
     }
 
     // paper ids to paper objects
     mapping(uint => Paper) private papers;
-    uint[] private ready;
-    uint[] private notReady; //ids of papers not ready to be reviewed
+    uint[] storage private ready; // papers ready to be reviewed [dynamically sized storage array [searched from the solidity documentation]]
 
     mapping(address => uint) private inProgress; //limiting the number of papers to 1 per person for now
 
-    uint limit;
+    uint limit; // if limit is reached then it is ready to be reviewed
     uint currentId; //next id to be assigned
 
     // constructor
@@ -28,19 +28,38 @@ contract Review {
         currentId = 0;
     }
 
-    function addPaper(string memory _title, string memory _location, ) private { // whats the thing with memory again?
-
+    function addPaper(string memory _title, string memory _location) public { // whats the syntax with memory again?
+        require(inProgress[msg.sender] == 0) //semicolon? makes sure that person adding paper does not have another paper curently in review
+        papers[currentId] = Paper(currentId, _location, _title, 0, msg.sender, false);
+        currentId++;
     }
 
-    function stake(uint _id) public onlyOnce {
-        papers[_id].numTokens++;
+    function stake(uint _id) public {
+        require(papers[_id].ready == false) //must not currently be in review
+        Paper p = papers[_id];
+        p.numTokens += tx.gasprice; // value of tokens stake depends on gas price of operation
 
-        if (papers[_id].numTokens >= limit) {
-            notReady.
+        if (p.numTokens >= limit) { //if limit reached, it is ready to be reviewed
+            ready.push(_id);
+            p.ready = true;
         }
     }
 
     //getter methods
-    
+    function getLimit() public returns (uint) {
+        return limit;
+    }
+    function isReady(_id) public returns (bool) {
+        return paper[_id].ready;
+    }
+    function getAuthor(_id) public returns (string) {
+        return paper[_id].author;
+    }
+    function getTitle(_id) public returns (string) {
+        return paper[_id].title;
+    }
+    function getNumTokens(_id) public returns (uint) {
+        return paper[_id].numTokens;
+    }
 
 }
