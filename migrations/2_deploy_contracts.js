57b6940468a3c52d1af1ff9cf0ae1bf6b4a86b6c
Olivia Lee <olivia.suji.lee@berkeley.edu>
2019-05-01 03:24:02 -0700

    partial testing for crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,13 @@
 var Review = artifacts.require("../contracts/Review.sol");
 var Token = artifacts.require("../contracts/CAJCoin.sol");
-//var Storage = artifacts.require("./SimpleStorage.sol");
 var Crowdsale = artifacts.require("../contracts/Crowdsale.sol")
 
 module.exports = function(deployer) {
   deployer.deploy(Review);
-  // deployer.deploy(Token);
   deployer.deploy(Token);
   deployer.deploy(Crowdsale,
-    1, 
+    1, //exchange rate currently set as 1-to-1
     "0xf0b55ecaeea488c91254e9293d1b6b750dedb299", // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address. 
-    // 2000000000000000000, // 2 ETH
-    // 500000000000000000000 // 500 ETH
     Token.address
   );
 };
