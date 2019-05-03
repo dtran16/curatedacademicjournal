ff66c9121882097f9a78381d121137f8f9afbffd
joe-broder15 <joe.broder@berkeley.edu>
2019-05-02 18:39:56 -0700

    fixed deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+//import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
+import ReviewContract from "./contracts/Review.json"
 
 import "./App.css";
 
@@ -17,9 +18,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = ReviewContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        ReviewContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -39,10 +40,10 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(2).send({ from: accounts[0] });
+    //await contract.methods.set(2).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    const response = await contract.methods.getCurrentID().call().toNumber();//.call();
 
     // Update state with the result.
     this.setState({ storageValue: response });
