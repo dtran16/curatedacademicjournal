4ee0a010d60f94b8a417c2ae3d9198106ba3f67c
joe-broder15 <joe.broder@berkeley.edu>
2019-05-03 00:09:02 -0700

    removed deployment for tokensale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,9 +5,9 @@ var Crowdsale = artifacts.require("../contracts/Crowdsale.sol")
 module.exports = function(deployer) {
   deployer.deploy(Review);
   deployer.deploy(Token);
-  deployer.deploy(Crowdsale,
-    1, //exchange rate currently set as 1-to-1
-    "0xf0b55ecaeea488c91254e9293d1b6b750dedb299", // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address. 
-    Token.address
-  );
+  // deployer.deploy(Crowdsale,
+  //   1, //exchange rate currently set as 1-to-1
+  //   "0xf0b55ecaeea488c91254e9293d1b6b750dedb299", // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address. 
+  //   Token.address
+  // );
 };
