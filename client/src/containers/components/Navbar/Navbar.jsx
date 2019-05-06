b54805ef8817116a85c29ceb0cfcf8e3564f9b4e
Olivia Lee <olivia.suji.lee@berkeley.edu>
2019-05-06 12:57:05 -0700

    fix navbar link to metamask

--- client/src/containers/components/Navbar/Navbar.jsx
+++ client/src/containers/components/Navbar/Navbar.jsx
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Navbar.css";
 
 import profile from './profile.png';
 
-class Navbar extends Component {
-    constructor(Props) {
-        super(Props);
-    }
-
-    render () {
-        
-        return (
-            <div className="rowContainer">
-                <div className="logoName leftAnchor" onClick={() => get(this.Props.accounts, this.Props.helper)}>Indelve</div>
-                <div className="rightAnchor">
-                    <a className="upload" href="http://localhost:3000/upload">upload</a>
-                    <div className="profile" >
-                    <a href="http://www.yahoo.com">
-                        <img src={profile}
-                            alt="Profile"
-                            height="50px"
-                            width="50px"/>
-                    </a>
-                </div>
-                </div>
+const Navbar = (Props) => {
+    return (
+        <div className="rowContainer">
+            <div className="logoName leftAnchor" onClick={() => get(Props.accounts, Props.helper)}>Indelve</div>
+            <div className="rightAnchor">
+                <a className="upload" href="http://localhost:3000/upload">upload</a>
+                <div className="profile" >
+                <a href="http://www.yahoo.com">
+                    <img src={profile}
+                        alt="Profile"
+                        height="50px"
+                        width="50px"/>
+                </a>
             </div>
-        )   
-    }
+            </div>
+        </div>
+    )   
 }
 
 async function get(acct, helper) {
